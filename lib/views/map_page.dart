f1e4a2f2813a2c969485185d8f1dc956df8f0e60
German234 <00059122@uca.edu.sv>
2024-06-09 17:02:27 -0600

    Adding map to the page :lipstick:

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Map Page'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(13.6811406, -89.235873),
+          initialZoom: 18.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+          RichAttributionWidget(
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('OpenStreetMap contributors'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
