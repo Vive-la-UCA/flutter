837d1dea3b44f0d500484a65a455a207c3181b26
jarlexdev <jarlexdev@gmail.com>
2024-06-10 22:06:27 -0600

    :sparkles: add geolocator and manifiest dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,32 @@ import 'package:vive_la_uca/views/register_page.dart';
 import 'package:vive_la_uca/views/splash_screen.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-   MyApp({super.key});
+  MyApp({super.key});
   final _routes = {
-        '/': (context) => const SplashScreen(),
-        '/home': (context)=> const HomePage(),
-        '/login': (context) =>  LoginPage(),
-        '/register': (context) =>  const RegisterPage(),
-
-      }; 
+    '/': (context) => const SplashScreen(),
+    '/home': (context) => const HomePage(),
+    '/login': (context) => LoginPage(),
+    '/register': (context) => const RegisterPage(),
+  };
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      routes: _routes,
-      onGenerateRoute: (settings){
-        return MaterialPageRoute(builder: (context) => const HomePage());
-      }
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: _routes,
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(builder: (context) => const HomePage());
+        });
   }
 }
-
-
-
-
-  
--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,39 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Map'),
+        title: const Text('Map'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(13.6811406, -89.235873),
-          initialZoom: 18.0,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-          RichAttributionWidget(
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('OpenStreetMap contributors'),
-                    ),
-                  );
-                },
+      body: FutureBuilder<Position>(
+        future: _getCurrentLocation(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final position = snapshot.data!;
+            final LatLng currentLocation =
+                LatLng(position.latitude, position.longitude);
+
+            return FlutterMap(
+              options: MapOptions(
+                initialCenter: currentLocation,
+                initialZoom: 18.0,
               ),
-            ],
-          ),
-        ],
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: currentLocation,
+                      child: const Icon(
+                        Icons.location_pin,
+                        color: Colors.blue,
+                        size: 40.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: Text('Unable to determine location'));
+          }
+        },
       ),
     );
   }
