920c37fe723fe03435e9013b90fc8d299e6cdeab
German234 <00059122@uca.edu.sv>
2024-06-12 18:56:15 -0600

    Fixing bugs with the map

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-  Stream<Position> _getPositionStream() {
-    return Geolocator.getPositionStream(
-      locationSettings: const LocationSettings(
-        accuracy: LocationAccuracy.bestForNavigation,
-        distanceFilter: 1,
-      ),
-    );
+  LatLng? currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
   }
 
-  Future<void> _checkPermission() async {
+  Future<void> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
+    // Verificar si los servicios de localización están habilitados.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
+      // Servicios de localización no habilitados.
       return Future.error('Location services are disabled.');
     }
 
@@ -33,20 +33,22 @@ class _MapPageState extends State<MapPage> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
+        // Permisos de localización denegados.
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
+      // Permisos de localización denegados permanentemente.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-  }
 
-  @override
-  void initState() {
-    super.initState();
-    _checkPermission();
+    // Obtener la ubicación actual del dispositivo.
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      currentLocation = LatLng(position.latitude, position.longitude);
+    });
   }
 
   @override
@@ -55,21 +57,11 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: StreamBuilder<Position>(
-        stream: _getPositionStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            final position = snapshot.data!;
-            final LatLng currentLocation =
-                LatLng(position.latitude, position.longitude);
-
-            return FlutterMap(
+      body: currentLocation == null
+          ? const Center(child: CircularProgressIndicator())
+          : FlutterMap(
               options: MapOptions(
-                initialCenter: currentLocation,
+                initialCenter: currentLocation!,
                 initialZoom: 18.0,
               ),
               children: [
@@ -77,27 +69,22 @@ class _MapPageState extends State<MapPage> {
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   userAgentPackageName: 'com.example.app',
                 ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: currentLocation,
-                      child: const Icon(
+                CurrentLocationLayer(
+                  style: const LocationMarkerStyle(
+                    marker: DefaultLocationMarker(
+                      child: Icon(
                         Icons.circle,
                         color: Colors.red,
                         size: 20.0,
                       ),
                     ),
-                  ],
+                    markerSize: Size(20, 20),
+                    markerDirection: MarkerDirection.heading,
+                    accuracyCircleColor: Colors.transparent, // Elimina el borde azul
+                  ),
                 ),
               ],
-            );
-          } else {
-            return const Center(child: Text('Unable to determine location'));
-          }
-        },
-      ),
+            ),
     );
   }
 }
