af7d2e42291b28118fa6c9ba3c7b0251399648c2
christian-lopezxd <00179320@uca.edu.sv>
2024-06-16 18:59:54 -0600

    :hammer: Rework: Edit text labels in lobby page

--- lib/widgets/simple_text.dart
+++ lib/widgets/simple_text.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 class SimpleText extends StatelessWidget {
   final String text;
   final double fontSize;
+  final String fontFamily;
   final FontWeight fontWeight;
+  final TextAlign textAlign;
   final Color color;
 
   const SimpleText({
     super.key,
     required this.text,
     this.fontSize = 24.0,
+    this.fontFamily = 'Montserrat',
     this.fontWeight = FontWeight.bold,
     this.color = Colors.white,
+    this.textAlign = TextAlign.center,
   });
 
   @override
@@ -21,9 +25,11 @@ class SimpleText extends StatelessWidget {
       style: TextStyle(
         fontSize: fontSize,
         fontWeight: fontWeight,
-        fontFamily: 'Montserrat',
+        fontFamily: fontFamily,
         color: color,
+        
       ),
+      textAlign: textAlign,
     );
   }
 }
