0f0b14c70df70252b5f2e24865deb4f56fab9d37
christian-lopezxd <00179320@uca.edu.sv>
2024-06-18 01:39:32 -0600

    :sparkles: Add: Location Services

---
+++ lib/services/location_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LocationService {
+  final  baseUrl;
+
+  LocationService({required this.baseUrl});
+
+  Future<List<dynamic>> getRoutes(tokenKey) async {
+    final url = Uri.parse('$baseUrl/api/location');
+    final response = await http.get(
+      url,
+      headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $tokenKey'},
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['location']; // Devuelve directamente la lista de rutas
+    } else {
+      throw Exception('Failed to load locations');
+    }
+  }
+}
