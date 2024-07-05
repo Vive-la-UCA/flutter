3c167c44b5dfa4ef60637609df3b61038a31d748
christian-lopezxd <00179320@uca.edu.sv>
2024-07-05 01:22:02 -0600

    :bug: Fix: Redirect problem

---
+++ lib/views/route_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RouteInfo extends StatefulWidget {
+  const RouteInfo({super.key});
+
+  @override
+  State<RouteInfo> createState() => _RouteInfoState();
+}
+
+class _RouteInfoState extends State<RouteInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
