a760643d57cb98f56fa93f52284291aa3843a27c
JosseJuventino <00048322@uca.edu.sv>
2024-07-09 20:52:10 -0600

    💄 Change the style of the button in navigation
    Making rounded the button of nav and some changes of design

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -14,23 +14,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
-
-   final List<Widget> _pages = [
-     const LobbyPage(),
+  final List<Widget> _pages = [
+    const LobbyPage(),
     const ProfilePage(),
     const MapPage(),
   ];
 
- void _onItemTapped(int index) {
+  void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;  
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  _pages.elementAt(_selectedIndex),
+      body: _pages.elementAt(_selectedIndex),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
         notchMargin: 6.0,
@@ -38,26 +37,39 @@ class _HomePageState extends State<HomePage> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-           
             InkWell(
               onTap: () => _onItemTapped(0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Icon(Icons.home, color: _selectedIndex == 0 ? Colors.orange : Colors.grey),
-                  Text('Inicio', style: TextStyle(fontFamily: 'Montserrat', color: _selectedIndex == 0 ? Colors.orange : Colors.grey), )
+                  Icon(Icons.home,
+                      color: _selectedIndex == 0 ? Colors.orange : Colors.grey),
+                  Text(
+                    'Inicio',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color:
+                            _selectedIndex == 0 ? Colors.orange : Colors.grey),
+                  )
                 ],
               ),
             ),
-           const SizedBox(width: 48),
-            
+            const SizedBox(width: 48),
             InkWell(
               onTap: () => _onItemTapped(1),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Icon(Icons.person, color: _selectedIndex == 1 ? Colors.orange : Colors.grey),
-                  Text('Perfil', style: TextStyle(fontFamily: 'Montserrat',color: _selectedIndex == 1 ? Colors.orange : Colors.grey))
+                  Icon(Icons.person,
+                      color: _selectedIndex == 1 ? Colors.orange : Colors.grey),
+                  Text('Perfil',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: _selectedIndex == 1
+                              ? Colors.orange
+                              : Colors.grey))
                 ],
               ),
             ),
@@ -66,9 +78,11 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _onItemTapped(2),
-        
-        backgroundColor: _selectedIndex == 2 ?  Colors.orange :  Theme.of(context).primaryColor,
-        child: const Icon(Icons.map_sharp, color: Colors.white),
+        shape: const CircleBorder(),
+        backgroundColor: _selectedIndex == 2
+            ? Colors.orange
+            : Theme.of(context).primaryColor,
+        child: const Icon(Icons.map_rounded, color: Colors.white),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
